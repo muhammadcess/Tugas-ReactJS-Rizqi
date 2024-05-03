b1e79100de99dd5e6a009756172212107aeef43a
muhamnmadcess <rizqismuhamad@gmail.com>
2024-05-03 15:19:19 +0700

    tugas react js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1 className="title">Hi, Saya Muhammad Rizqi</h1>
+        <hr></hr>
+        <p className="paragraph">
+          Saya dari Kelas 12 sudah berhasil menginstall React
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
