ac6fb2eb5d85d0b3799d41e62ff5c83306c70d65
muhamnmadcess <rizqismuhamad@gmail.com>
2024-05-20 14:39:17 +0700

    nambah todolist

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import "./App.css";
 import Tugas6 from "./Tugas6/tugas6";
 import Tugas7 from "./Tugas7/tugas7";
+import Tugas8 from "./Tugas8/tugas8";
 
 function App() {
   return (
     <div className="App">
       <Tugas6 name="Rizqi" />
+      
       <Tugas7
         namaLengkap="Muhammad Rizqi"
         email="muh52464@smk.belajar.id"
-        kelas="XII Sija"
-      />
+        kelas="XII Sija"/>
+      <Tugas8 />
     </div>
   );
 }
---
+++ src/Tugas8/tugas8.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Tugas8(props) {
+  return (
+    <div className="box">
+      <ul className="text-center">
+        <p>1</p>
+       <button className="w-100 button">tambah</button>
+      </ul>
+    </div>
+  );
+}
+
+export default Tugas8;
